430f8952cf5fdf69d10f216f70eae07a0ecc433b
epimethix <epimethix@protonmail.com>
2023-09-01 22:44:27 +0200

    improved

--- swing/src/main/java/com/github/epimethix/lumicore/swing/list/JListModel.java
+++ swing/src/main/java/com/github/epimethix/lumicore/swing/list/JListModel.java
@@ -1,10 +1,13 @@
-package com.github.epimethix.lumicore.swing.control;
+package com.github.epimethix.lumicore.swing.list;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.swing.AbstractListModel;
 
@@ -13,7 +16,10 @@ public class JListModel<E> extends AbstractListModel<E> {
 
 	private Comparator<E> comparator;
 
+	private Function<E, Boolean> filter;
+
 	private List<E> data = new ArrayList<>();
+	private List<E> dataFiltered = new ArrayList<>();
 
 	public void setComparator(Comparator<E> comparator) {
 		this.comparator = comparator;
@@ -25,18 +31,34 @@ public class JListModel<E> extends AbstractListModel<E> {
 		}
 	}
 
+	private void filter() {
+		dataFiltered.clear();
+		if (Objects.nonNull(filter)) {
+			dataFiltered.addAll(data.stream().filter(e -> filter.apply(e)).collect(Collectors.toList()));
+		} else {
+			dataFiltered.addAll(data);
+		}
+	}
+
+	public void filter(Function<E, Boolean> filter) {
+		this.filter = filter;
+		filter();
+		fireContentsChanged(this, 0, getSize());
+	}
+
 	@Override
 	public int getSize() {
-		return data.size();
+		return dataFiltered.size();
 	}
 
 	@Override
 	public E getElementAt(int index) {
-		return data.get(index);
+		return dataFiltered.get(index);
 	}
 
 	public void setElementAt(int selectedIndex, E e2) {
 		data.set(selectedIndex, e2);
+		filter();
 		sort();
 		fireContentsChanged(this, selectedIndex, selectedIndex + 1);
 	}
@@ -45,16 +67,44 @@ public class JListModel<E> extends AbstractListModel<E> {
 		return new ArrayList<>(data);
 	}
 
+	public List<E> getData(int[] indices) {
+		ArrayList<E> result = new ArrayList<>();
+		for (int i = 0; i < indices.length; i++) {
+			result.add(data.get(indices[i]));
+		}
+		return data;
+	}
+
 	public void setData(List<E> data) {
-		this.data = new ArrayList<>(data);
+		if (Objects.isNull(data)) {
+			this.data = new ArrayList<>();
+		} else {
+			this.data = new ArrayList<>(data);
+		}
+		filter();
 		sort();
 		fireContentsChanged(this, 0, getSize());
 	}
 
+	public void addDatum(E datum) {
+		data.add(datum);
+		filter();
+		sort();
+		fireContentsChanged(this, getSize() - 1, getSize());
+	}
+
 	public void addDatum(int index, E datum) {
 		data.add(index, datum);
+		filter();
 		sort();
 		fireContentsChanged(this, index, getSize());
+	}
+
+	public void addData(List<E> data) {
+		this.data.addAll(data);
+		filter();
+		sort();
+		fireContentsChanged(this, 0, getSize());
 	}
 
 	public void removeDatum(E datum) {
@@ -64,22 +114,26 @@ public class JListModel<E> extends AbstractListModel<E> {
 	public void removeDatum(int index) {
 		if (index > -1 && index < data.size()) {
 			data.remove(index);
+			filter();
 			fireContentsChanged(this, index, getSize());
 		}
 	}
 
-	public void addDatum(E datum) {
-		data.add(datum);
-		sort();
-		fireContentsChanged(this, getSize() - 1, getSize());
+	public void removeData(Collection<E> data) {
+		this.data.removeAll(data);
+		filter();
+		fireContentsChanged(this, 0, getSize());
 	}
 
 	public void clear() {
 		data.clear();
+		filter();
 		fireContentsChanged(this, 0, getSize());
 	}
 
 	public void refresh() {
+		sort();
+		filter();
 		fireContentsChanged(this, 0, getSize());
 	}
 }
