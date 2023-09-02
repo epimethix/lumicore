9bcee62cc4ccce830ba6aa0206ff0e9686a60fb4
epimethix <epimethix@protonmail.com>
2023-09-02 17:25:49 +0200

    set dark theme

--- dev-tools/src/main/java/com/github/epimethix/lumicore/devtools/AppProperties.java
+++ dev-tools/src/main/java/com/github/epimethix/lumicore/devtools/AppProperties.java
@@ -1,13 +1,12 @@
 package com.github.epimethix.lumicore.devtools;
 
 import com.github.epimethix.lumicore.properties.ApplicationProperties;
+import com.github.epimethix.lumicore.properties.Theme;
 
 public class AppProperties extends ApplicationProperties {
 
 	public AppProperties(String propertiesName) {
 		super(propertiesName);
+		setTheme(Theme.DARK);
 	}
-	
-	
-
 }
